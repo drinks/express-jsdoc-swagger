f63896b25a451f3d305faa68244287788946c1d4
Dan Drinkard <dan.drinkard@customink.com>
2022-10-13 18:45:53 -0400

    fix: make route handlers wait for swagger to process

--- index.js
+++ index.js
@@ -9,48 +9,63 @@ const swaggerEvents = require('./swaggerEvents');
 const expressJSDocSwagger = app => (userOptions = {}, userSwagger = {}) => {
   const events = swaggerEvents(swaggerEventsOptions(userOptions));
   const { instance } = events;
-  let swaggerObject = {};
-
   const options = {
     ...defaultOptions,
     ...userOptions,
   };
+  let swaggerObject;
 
-  processSwagger(options, events.processFile)
-    .then(result => {
-      swaggerObject = {
-        ...swaggerObject,
-        ...result.swaggerObject,
-      };
-      swaggerObject = merge.recursive(true, swaggerObject, userSwagger);
-      events.finish(swaggerObject, {
-        jsdocInfo: result.jsdocInfo,
-        getPaths: result.getPaths,
-        getComponents: result.getComponents,
-        getTags: result.getTags,
-      });
+  const initSwaggerObject = () => (
+    new Promise((resolve, reject) => {
+      if (swaggerObject) {
+        resolve(swaggerObject);
+      } else {
+        processSwagger(options, events.processFile)
+          .then(result => {
+            swaggerObject = {
+              ...swaggerObject,
+              ...result.swaggerObject,
+            };
+            swaggerObject = merge.recursive(true, swaggerObject, userSwagger);
+            events.finish(swaggerObject, {
+              jsdocInfo: result.jsdocInfo,
+              getPaths: result.getPaths,
+              getComponents: result.getComponents,
+              getTags: result.getTags,
+            });
+            resolve(swaggerObject);
+          })
+          .catch(err => {
+            events.error(err);
+            reject(err);
+          });
+      }
     })
-    .catch(events.error);
+  );
+  initSwaggerObject();
 
   if (options.exposeSwaggerUI) {
     app.use(options.swaggerUIPath, (req, res, next) => {
-      swaggerObject = {
-        ...swaggerObject,
-        host: req.get('host'),
-      };
-      req.swaggerDoc = swaggerObject;
-      next();
+      initSwaggerObject().then(spec => {
+        req.swaggerDoc = {
+          ...spec,
+          host: req.get('host'),
+        };
+        next();
+      }).catch(next);
     }, swaggerUi.serve, swaggerUi.setup(undefined, options.swaggerUiOptions));
   }
 
   if (options.exposeApiDocs) {
-    app.get(options.apiDocsPath, (req, res) => {
-      res.json({
-        ...swaggerObject,
-        // we skipped this as is not a valid prop in OpenAPI
-        // This is only being used in the SwaggerUI Library
-        host: undefined,
-      });
+    app.get(options.apiDocsPath, (req, res, next) => {
+      initSwaggerObject().then(spec => {
+        res.json({
+          ...spec,
+          // we skipped this as is not a valid prop in OpenAPI
+          // This is only being used in the SwaggerUI Library
+          host: undefined,
+        });
+      }).catch(next);
     });
   }
 
